123a94ab0601d6c645900a0d50a83767aa4b8054
mohammad70623 <mohammad70623@gmail.com>
2026-01-31 23:59:49 +0600

    add groq api key loaded

--- groq_voice_assistant.py
+++ groq_voice_assistant.py
@@ -6,4 +6,7 @@ from dotenv import load_dotenv
 import tempfile
 import pyttsx3
 
-  
+# Load API key
+load_dotenv()
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+client = Groq(api_key=GROQ_API_KEY)
